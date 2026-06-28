129911f09beb6a3f0fb55dc2e6261b74a9ee31f7
Hitarth gohel <hitarthgohel15@gmail.com>
2026-06-28 09:12:05 +0530

    feat(firebase): initialize Firebase and Configure FlutterFire

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hf_1/firebase_options.dart';
 
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const LumoraApp());
 }
 
